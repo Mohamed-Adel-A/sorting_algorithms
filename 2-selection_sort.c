07c8196a6efa674e7c52b636777c138406d16e5d
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-25 00:06:50 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,7 +16,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0 ; i < size ; i++)
 	{
 		current = i;
-		for(j = i + 1 ; j < size ; j++)
+		for (j = i + 1 ; j < size ; j++)
 		{
 			if (array[current] > array[j])
 			{
