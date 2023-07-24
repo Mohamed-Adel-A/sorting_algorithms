8957d9fcabaf71ec0efa04cab00861a97e12f827
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-24 14:34:17 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,7 +4,6 @@ void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node)
 {
 	listint_t *tmp_node;
 
-	tmp_node->n = first_node->n;
 	tmp_node->next = first_node->next;
 	tmp_node->prev = first_node->prev;
 
@@ -16,11 +15,9 @@ void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node)
 	if (second_node->next != NULL)
 		second_node->next->prev = first_node;
 	
-	first_node->n = second_node->n;
 	first_node->next = second_node->next;
 	first_node->prev = second_node->prev;
 
-	second_node->n = first_node->n;
 	second_node->next = first_node->next;
 	second_node->prev = first_node->prev;
 
