a0b20f8cf2ed302f628af5014283e557c317d3bc
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-24 14:25:14 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,28 @@
 #include "sort.h"
 
+void swap_nodes(listint_t *first_node, listint_t *second_node)
+{
+	listint_t *tmp_node;
+
+	tmp_node->n = first_node->n;
+	tmp_node->next = first_node->next;
+	tmp_node->prev = first_node->prev;
+
+	if (first_node->prev != NULL)
+		first_node->prev->next = second_node;
+	if (second_node->next != NULL)
+		second_node->next->prev = first_node;
+	
+	first_node->n = second_node->n;
+	first_node->next = second_node->next;
+	first_node->prev = second_node->prev;
+
+	second_node->n = first_node->n;
+	second_node->next = first_node->next;
+	second_node->prev = first_node->prev;
+
+	
+}
 
 /**
  * insertion_sort_list - function that sorts a doubly linked list of integers in ascending order
@@ -10,5 +33,23 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	
+	listint_s *current_node, *next_node, *head = *list;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	current_node = head;
+	next_node = head->next;
+	while(next_node != NULL)
+	{
+		while(current_node->prev != NULL)
+		{
+			if (current_node->n < curent_node->prev->n)
+			{
+				swap_node(curent_node->prev, current_node);
+				current_node = current_node->prev;
+				print_list(*list);
+			}
+		}
+	}	
 }
