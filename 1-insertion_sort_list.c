1f60bc8e6c7b4d6e39cb85bfc0a5370882a5638a
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-24 21:02:37 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+/**
+ * swap_nodes - swap node in list
+ * @head: list head
+ * @first_node: first node
+ * @second_node: second node
+ *
+ * Return: nothig (void)
+ */
 void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node)
 {
 	if (first_node->prev != NULL)
@@ -9,7 +17,7 @@ void swap_nodes(listint_t **head, listint_t *first_node, listint_t *second_node)
 
 	if (second_node->next != NULL)
 		second_node->next->prev = first_node;
-	
+
 	first_node->next = second_node->next;
 	second_node->next = first_node;
 
@@ -33,9 +41,9 @@ void insertion_sort_list(listint_t **list)
 
 	current_node = head;
 	next_node = head;
-	while(next_node != NULL)
+	while (next_node != NULL)
 	{
-		while(current_node->prev != NULL)
+		while (current_node->prev != NULL)
 		{
 			if (current_node->n < current_node->prev->n)
 			{
@@ -47,5 +55,5 @@ void insertion_sort_list(listint_t **list)
 		}
 		next_node = next_node->next;
 		current_node = next_node;
-	}	
+	}
 }
