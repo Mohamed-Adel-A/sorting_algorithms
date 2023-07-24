6020042ebe33c627981c06ccd2a321095b3db4d8
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-25 00:16:13 +0300

    Create 3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * quick_sort - a function that sorts an array of integers
+ *       in ascending order using the Quick sort algorithm
+ * @array: array to be sorted
+ * @size: array size
+ *
+ * Return: nothing (void)
+ */
+void quick_sort(int *array, size_t size)
+{
+	
+}
