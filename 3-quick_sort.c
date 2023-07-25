778a53b484636ed6ba7fd0621a710461ab818416
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-25 22:47:37 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,35 @@
 #include "sort.h"
+
+
+/**
+ * swap_elements - swap two elements
+ * @first: first element
+ * @second: second element
+ *
+ * Return: void
+ */
+void swap_elements(int *first, int *second)
+{
+	int tmp;
+
+	tmp = *first;
+	*first = *second;
+	*second = tmp;
+}
+
+
+/**
+ * quick_recursion: order element recursivly
+ * high: high index
+ * low: low index
+ *
+ * Return: void
+ */
+void quick_recursion(int *array, int high, int low)
+{
+	
+}
+
 /**
  * quick_sort - a function that sorts an array of integers
  *       in ascending order using the Quick sort algorithm
@@ -9,5 +40,5 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	
+	quick_recursion(array, 0, size)
 }
