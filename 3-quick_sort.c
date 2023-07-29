279deb2408a1064ad41fe71a2c641fd0a29e8632
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-29 17:59:28 +0300

    testing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -32,7 +32,7 @@ int quick_partition(int *array, size_t low, size_t high, size_t size)
 	size_t i = low, k = low;
 
 	pivotvalue = array[high];
-	printf("low = %i, high = %i, pivotvalue = %i\n", low, high, pivotvalue);
+	printf("low = %lu, high = %lu, pivotvalue = %i\n", low, high, pivotvalue);
 	for (i = low ; i < high ; i++)
 	{
 		/*
