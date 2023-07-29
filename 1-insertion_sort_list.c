3e5b8d7e4ecdf466cf8d1be49dcf7f512bdeed7a
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-07-29 17:02:24 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -37,7 +37,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current_node, *next_node, *head;
 
-	if (list == NULL || *list == NULL || head->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	head = *list;
